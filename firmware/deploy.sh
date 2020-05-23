ddfac1e399d0d4d07a06eb5789d35fca267e4527
Katona <katona>
2020-05-23 22:25:26 +0200

    Stop service for the duration of deployment

--- firmware/deploy.sh
+++ firmware/deploy.sh
@@ -6,4 +6,6 @@ HOST=dimmer@magic-wand.local
 rm -r dist
 venv/bin/python3 setup.py bdist_wheel
 scp dist/$PACKAGE_NAME $HOST:
+ssh $HOST sudo systemctl stop dimmer.service
 ssh $HOST venv/bin/pip3 install -I $PACKAGE_NAME
+ssh $HOST sudo systemctl start dimmer.service
