2b012835ea637bfc4b259600d5b08c23938fcc22
Katona <katona>
2020-05-25 19:53:14 +0200

    Bug fix: use calculated brightness value

--- firmware/flaskr/led_strip.py
+++ firmware/flaskr/led_strip.py
@@ -7,5 +7,5 @@ pi = pigpio.pi()
 max_duty_cicle = 1000000
 def update_brightness(percent):
     # output is inverted, so high duty cicle is actually darker ...
-    brightness_value = max_duty_cicle - max_duty_cicle * percent / 100
-    pi.hardware_PWM(pin, 20000, 80000)
+    brightness_value = int(max_duty_cicle - max_duty_cicle * percent / 100)
+    pi.hardware_PWM(pin, 20000, brightness_value)
