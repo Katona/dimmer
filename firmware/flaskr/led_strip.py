3c0201c5da5b07b46f93f6ee71b3962dd88d05bc
Katona <katona>
2020-06-02 14:18:27 +0200

    Introduce brightness animation

--- firmware/flaskr/led_strip.py
+++ firmware/flaskr/led_strip.py
@@ -1,11 +1,42 @@
 import pigpio
+import time
+from concurrent.futures import ThreadPoolExecutor, CancelledError
+from threading import Event
+from easing_functions import *
 
 pin = 18
 
 pi = pigpio.pi()
 
-max_duty_cicle = 1000000
+executor = ThreadPoolExecutor(max_workers=1)
+current_animation = None
+exit = None
+
+max_duty_cycle = 1000000
 def update_brightness(percent):
-    # output is inverted, so high duty cicle is actually darker ...
-    brightness_value = int(max_duty_cicle - max_duty_cicle * percent / 100)
-    pi.hardware_PWM(pin, 20000, brightness_value)
+    global current_animation
+    global exit
+
+    if current_animation is not None:
+        print("cancel", current_animation)
+        exit.set()
+    current_brightness = current_animation.result(1) if current_animation is not None else max_duty_cycle
+    
+    # output is inverted, so high duty cycle is actually darker ...
+    target_brightness = int(max_duty_cycle - max_duty_cycle * percent / 100)
+
+    exit = Event()
+    current_animation = executor.submit(brightness_animation_task, current_brightness, target_brightness, exit)
+
+
+def brightness_animation_task(initial_brightness, target_brightness, exit):
+    step_count = 50
+    brightness = initial_brightness
+    easing = QuadEaseInOut(start=initial_brightness, end = target_brightness, duration = step_count)
+    step = 0
+    while not exit.is_set() and step <= step_count:
+        brightness = easing.ease(step)
+        pi.hardware_PWM(pin, 20000, brightness)
+        step = step + 1
+        exit.wait(0.02)
+    return brightness
--- firmware/setup.py
+++ firmware/setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         'flask',
         'pigpio',
-        'waitress'
+        'waitress',
+        'easing-functions'
     ],
 )
